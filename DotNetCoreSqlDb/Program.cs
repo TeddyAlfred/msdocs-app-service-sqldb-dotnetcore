4cd2b63b69e0010cc4a8e1025412dc74d461c913
TeddyAlfred <39256161+TeddyAlfred@users.noreply.github.com>
2023-12-19 17:37:57 +0900

    comment out cache redis

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-// builder.Services.AddDistributedMemoryCache();
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-options.Configuration = builder.Configuration["REDIS_CONNECTIONSTRING"];
-options.InstanceName = "SampleInstance";
-});
+builder.Services.AddDistributedMemoryCache();
+// builder.Services.AddStackExchangeRedisCache(options =>
+// {
+// options.Configuration = builder.Configuration["REDIS_CONNECTIONSTRING"];
+// options.InstanceName = "SampleInstance";
+// });
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
